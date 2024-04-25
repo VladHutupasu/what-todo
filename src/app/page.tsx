dee8ce3b601249bbb760cfa20c78882314f4daa7
VladHutupasu <vlad.hutupasu@yahoo.com>
2024-04-26 00:18:28 +0200

    fix: force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import TodoListsOverview from '@features/TodoListsOverview/TodoListsOverview';
 import { getTodoLists } from '@server/db';
 
+export const dynamic = 'force-dynamic';
+// export const revalidate = 0; Same thing?
+
 export default async function Home() {
   const todoLists = await getTodoLists();
 
