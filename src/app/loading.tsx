7e04c22c06f0b8cf72fb081d7d2b4f853fb9ca20
VladHutupasu <vlad.hutupasu@yahoo.com>
2024-04-27 22:15:45 +0200

    feat: fix loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,10 +2,8 @@ import ClipboardIcon from '@heroicons/react/24/outline/ClipboardIcon';
 
 export default function Loading() {
   return (
-    <>
-      <div className="flex items-center justify-center h-full">
-        <ClipboardIcon className="animate-bounce h-8 w-8 text-primary" />
-      </div>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <ClipboardIcon className="animate-bounce h-8 w-8 text-primary" />
+    </div>
   );
 }
