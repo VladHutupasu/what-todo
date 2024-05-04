6986ab13a4f42148c93280646d956bc19ecae3b6
VladHutupasu <vlad.hutupasu@yahoo.com>
2024-05-04 10:09:17 +0200

    feat: another test onclick

--- src/components/features/TodoListDetail/MobileAddTodoItem.tsx
+++ src/components/features/TodoListDetail/MobileAddTodoItem.tsx
@@ -21,10 +21,22 @@ export default function MobileAddTodoItem({
   };
 
   setTimeout(() => {
-    if (inputRef.current) {
-      inputRef.current.value = 'Your value here';
-      inputRef.current.focus();
-    }
+    // if (inputRef.current) {
+    //   inputRef.current.value = 'Your value here';
+    //   inputRef.current.focus();
+    // }
+
+    var openButton = document.getElementById('open-search');
+    var closeButton = document.getElementById('close-search');
+    var searchBox = document.getElementById('search-box');
+    var searchInput = document.getElementById('search-input');
+    openButton!.onclick = function () {
+      searchBox!.style.display = 'block';
+      searchInput!.focus();
+    };
+    closeButton!.onclick = function () {
+      searchBox!.style.display = 'none';
+    };
   }, 3000);
 
   useEffect(() => {
@@ -61,6 +73,24 @@ export default function MobileAddTodoItem({
 
   return (
     <>
+      <div>
+        <a href="/" id="ms-outer" className="for-outer">
+          <img src="//ae01.alicdn.com/kf/HTB1O8URHFXXXXXPaXXX760XFXXXQ.png" alt="AE Logo" />
+        </a>
+        <div className="header-main">
+          <div id="open-search">open</div>
+        </div>
+      </div>
+
+      <div id="search-box">
+        <form action="/search.htm">
+          <div className="ms-autocomplete-main">
+            <input id="search-input" type="search" placeholder="search" name="keywords" />
+          </div>
+          <div id="close-search">close</div>
+        </form>
+      </div>
+
       {/* {isEditing && ( */}
       <div className="flex cursor-pointer label justify-start hover:bg-primary hover:bg-opacity-5 rounded">
         <input type="checkbox" className="checkbox checkbox-primary" />
